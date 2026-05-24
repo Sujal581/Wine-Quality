c837f258e5c2eba76f761375abe39f87a6a0de3f
Sujal581 <palsujal869@gmail.com>
2026-05-24 23:42:31 +0530

    Update 3_⚠️Sulfur & Chemical analysis.py

--- pages/3_⚠️Sulfur & Chemical analysis.py
+++ pages/3_⚠️Sulfur & Chemical analysis.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
 from style import (
     apply_futuristic_style, page_header, section_header, chart_label,
